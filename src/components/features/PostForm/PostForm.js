62231b4140868e43992e9676b2b5ba2ceb910d55
Mateusz Kretowski <matkretkurs@gmail.com>
2023-02-25 16:41:31 +0100

    Add validation for PostForm

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -6,6 +6,7 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import { useForm } from 'react-hook-form';
 
 const PostForm = ({ action, actionText, ...props }) => {
   const [title, setTitle] = useState(props.title || '');
@@ -13,48 +14,72 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || new Date());
   const [shortDescription, setShortDescription] = useState(props.shortDescription || '');
   const [content, setContent] = useState(props.content || '');
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
   const handleSubmit = () => {
-    action({ title, author, publishedDate, shortDescription, content });
+    setContentError(!content);
+    setDateError(!publishedDate);
+    if (content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content });
+    }
   };
+  const {
+    register,
+    handleSubmit: validate,
+    formState: { errors },
+  } = useForm();
+
   return (
     <Col className='col-md-8 col-12 offset-md-2 text-md-start text-center p-2'>
       <PageTitle>{actionText}</PageTitle>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={validate(handleSubmit)}>
         <Form.Group className='mb-3 col-md-6 col-12' controlId='title'>
           <Form.Label>Title</Form.Label>
           <Form.Control
+            {...register('title', { required: true, minLength: 3 })}
             type='text'
             placeholder='Enter title'
             value={title}
             onChange={(e) => setTitle(e.target.value)}
           />
+          {errors.title && <small className='d-block form-text text-danger mt-2'>Title is too short (min is 3)</small>}
         </Form.Group>
         <Form.Group className='mb-3 col-md-6 col-12' controlId='author'>
           <Form.Label>Author</Form.Label>
           <Form.Control
+            {...register('author', { required: true, minLength: 3 })}
             type='text'
             placeholder='Enter author'
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
           />
+          {errors.author && (
+            <small className='d-block form-text text-danger mt-2'>Name of author is too short (min is 3)</small>
+          )}
         </Form.Group>
         <Form.Group className='mb-3 col-md-6 col-12' controlId='date'>
           <Form.Label>Published</Form.Label>
           <DatePicker selected={publishedDate} onChange={(date) => setPublishedDate(date)} />
+          {dateError && <small className='d-block form-text text-danger mt-2'>Date can't be empty</small>}
         </Form.Group>
         <Form.Group className='mb-3' controlId='shortDescription'>
           <Form.Label>Short description</Form.Label>
           <Form.Control
+            {...register('shortDescription', { required: true, minLength: 3 })}
             as='textarea'
             rows={3}
             placeholder='Leave a comment here'
             value={shortDescription}
             onChange={(e) => setShortDescription(e.target.value)}
           />
+          {errors.shortDescription && (
+            <small className='d-block form-text text-danger mt-2'>Short description is too short (min is 3)</small>
+          )}
         </Form.Group>
         <Form.Group className='mb-3' controlId='mainContent'>
           <Form.Label>Main content</Form.Label>
           <ReactQuill theme='snow' value={content} onChange={setContent} placeholder='Leave a comment here' />
+          {contentError && <small className='d-block form-text text-danger mt-2'>Content can't be empty</small>}
         </Form.Group>
         <Button variant='primary' type='submit'>
           {actionText}
